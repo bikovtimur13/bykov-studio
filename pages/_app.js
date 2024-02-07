a9da4d870c281a40d74c1b7d81dae3e6b4f19b9d
Timur Bykov (4538) <bikov.timur.013@gmail.com>
2024-02-07 12:23:48 +0300

    feat: BS-003 - Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
+import Header from '@/src/components/Header/Header';
 
 export default function App({ Component, pageProps }) {
   return <>
           <Component {...pageProps} />
+          <Header/>
         </>;
-}
+};
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+import styles from './Header.module.scss';
+
+
+/** внешний футер сайта */
+const Header = () => {
+    return (<>
+        Header
+    </>);
+}
+
+export default Header;
